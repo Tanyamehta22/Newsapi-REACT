d5a5603204793047eccbe2edb7c0ffd62ecad447
Tanyamehta22 <127906792+Tanyamehta22@users.noreply.github.com>
2023-08-29 12:15:20 +0530

    VIDEO-23

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import Navbar from './components/Navbar';
+import News from './components/News';
 
 export default class App extends Component {
-  c= 'John'
+   
   render() {
     return (
       <div>
-        Hello my first class base Component {this.c}
+        <Navbar/>
+        <News/>
       </div>
     )
   }
